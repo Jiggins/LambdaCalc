21e6ff3134918cfd997fbfd1f16ed246714a3752
Jack Higgins <jackhiggins07@gmail.com>
2016-12-30 19:43:54 +0000

    Clean up of Lambda printing

--- src/Lambda.hs
+++ src/Lambda.hs
@@ -50,15 +50,15 @@ parensIf True = parens
 parensIf False = id
 
 instance Pretty Name where
-  ppr _ x = text x
+  ppr _ = text
 
 instance Pretty Expr where
-  ppr _ (Variable x)         = text x
+  ppr _ (Variable x)        = text x
   ppr _ (Literal (LInt a))  = text (show a)
   ppr _ (Literal (LBool b)) = text (show b)
-  ppr p e@(Application _ _)     = parensIf (p>0) (ppr p f <+> sep (map (ppr (p+1)) xs))
+  ppr p e@(Application _ _) = parensIf (p > 0) (ppr p f <+> sep (map (ppr (p + 1)) xs))
     where (f, xs) = viewApp e
-  ppr p e@(Lambda _ _)     = parensIf (p>0) $ char '\\' <> hsep vars <+> text "." <+> body
+  ppr p e@(Lambda _ _)      = parensIf (p > 0) $ char 'λ' <+> hsep vars <+> text "." <+> body
     where
       vars = map (ppr 0) (viewVars e)
       body = ppr (p+1) (viewBody e)
